17a1731308c22d7032a37843988a474563eb92ac
CrazyProger1 <crazyproger1@gmail.com>
2022-06-30 14:16:06 +0300

    Server: Fix: server startup

--- server/server/settings.py
+++ server/server/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        # 'PASSWORD': '123456',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': '5432',
     }
