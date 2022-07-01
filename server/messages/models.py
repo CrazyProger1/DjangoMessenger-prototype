939e9b232a72c0af7276a25eb430dcbba6214f98
CrazyProger1 <crazyproger1@gmail.com>
2022-07-01 16:16:45 +0300

    Server: Add: message model

--- server/messages/models.py
+++ server/messages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from bots.models import Bot
+from chats.models import *
 
-# Create your models here.
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, models.CASCADE, verbose_name='chat')
+    sender = models.ForeignKey(ChatMember, models.CASCADE, verbose_name='message sender')
+    type = models.CharField(verbose_name='message type', max_length=10,
+                            choices=('file', 'text', 'photo', 'document', 'executable'))
+    text = models.BinaryField(verbose_name='message text')
+    files_password = models.BinaryField(verbose_name='files password')
+    encryption_type = models.CharField(verbose_name='message encryption', max_length=10)
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+
+    def __str__(self):
+        return f'Message<>'
