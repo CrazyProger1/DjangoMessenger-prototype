0c6511d90d9108bbb598d0d2cfbfeed2933c71ca
CrazyProger1 <crazyproger1@gmail.com>
2022-07-01 11:10:51 +0300

    Server: Add: user profile model

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, models.CASCADE, unique=True, verbose_name='user')
+    # photo = models.ForeignKey('File', models.CASCADE, null=True, verbose_name='user photo')
+    api_key = models.BinaryField(verbose_name='api key')
+
+    class Meta:
+        verbose_name = 'User profile'
+        verbose_name_plural = 'User profiles'
+
+    def __str__(self):
+        return f'{self.user.username}'
