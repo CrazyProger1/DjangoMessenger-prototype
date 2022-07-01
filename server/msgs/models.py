50849a3a0e584522fe772e7c78c335466b4a15c1
CrazyProger1 <crazyproger1@gmail.com>
2022-07-01 17:05:10 +0300

    Server: Fix: message type choices

--- server/msgs/models.py
+++ server/msgs/models.py
@@ -7,8 +7,7 @@ from chats.models import *
 class Message(models.Model):
     chat = models.ForeignKey(Chat, models.CASCADE, verbose_name='chat')
     sender = models.ForeignKey(ChatMember, models.CASCADE, verbose_name='message sender')
-    type = models.CharField(verbose_name='message type', max_length=10,
-                            choices=('file', 'text', 'photo', 'document', 'executable'))
+    type = models.CharField(verbose_name='message type', max_length=10)
     text = models.BinaryField(verbose_name='message text')
     files_password = models.BinaryField(verbose_name='files password')
     encryption_type = models.CharField(verbose_name='message encryption', max_length=10, default='RSA')
