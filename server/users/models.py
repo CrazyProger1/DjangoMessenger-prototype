1025ae5c7814641afe8ab1742aefb21d01a3be62
CrazyProger1 <crazyproger1@gmail.com>
2022-07-01 11:36:10 +0300

    Server: Fix: user profile

--- server/users/models.py
+++ server/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
@@ -10,6 +12,17 @@ class UserProfile(models.Model):
     class Meta:
         verbose_name = 'User profile'
         verbose_name_plural = 'User profiles'
+
+    @staticmethod
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @staticmethod
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
 
     def __str__(self):
         return f'{self.user.username}'
