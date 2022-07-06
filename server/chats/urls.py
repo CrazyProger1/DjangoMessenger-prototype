a3c9200d80109bee35123a79a53c076eff6d0e28
CrazyProger1 <crazyproger1@gmail.com>
2022-07-06 10:54:18 +0300

    Server: Update: chat urls

--- server/chats/urls.py
+++ server/chats/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
+
+from rest_framework import routers
+
 from .views import *
 
-urlpatterns = [
-    path('', ChatViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('<int:pk>/', ChatViewSet.as_view({
-        'get': 'retrieve',
-        'patch': 'partial_update',
-        'put': 'update',
-        'delete': 'destroy'
-    }))
-]
+router = routers.SimpleRouter()
+router.register('', ChatViewSet)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', ChatViewSet.as_view({'get': 'list', 'post': 'create'})),
+#     path('<int:pk>/', ChatViewSet.as_view({
+#         'get': 'retrieve',
+#         'patch': 'partial_update',
+#         'put': 'update',
+#         'delete': 'destroy'
+#     }))
+# ]
