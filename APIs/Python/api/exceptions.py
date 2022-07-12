2eda69e9f4eec27d11158538edd3d28182c2eb58
CrazyProger1 <crazyproger1@gmail.com>
2022-07-12 11:46:24 +0300

    Python user api: Add: tests run script

--- APIs/Python/api/exceptions.py
+++ APIs/Python/api/exceptions.py
@@ -8,7 +8,10 @@ class InvalidHostError(Exception):
 
 class BaseAPIError(Exception):
     def __init__(self, error_key, error_text):
-        super(BaseAPIError, self).__init__(error_key + ': ' + error_text)
+        if error_key == 'detail':
+            super(BaseAPIError, self).__init__(error_text)
+        else:
+            super(BaseAPIError, self).__init__(error_key + ': ' + error_text)
 
 
 class WrongDataProvidedError(BaseAPIError):
